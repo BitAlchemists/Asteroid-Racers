0db191dd13b8a2b78556302815ca236676de4715
Tommi Enenkel <tommi.enenkel@gmail.com>
2014-05-18 11:02:34 +0200

    add runtest binary file

---
+++ bin/run_test.dart
@@ -0,0 +1,16 @@
+import "../lib/server/ar_server.dart" as ar_server;
+import "dart:io";
+import "package:path/path.dart" as path;
+
+main() {
+    ar_server.runServer(path.join(Directory.current.path, "../web"), null, 1337).then((value){
+      Process.start('explorer', ["http://localhost:1337/asteroidracers.html"]).then((Process process) {
+        //process.stdout
+        //  .transform(UTF8.decoder)
+        //  .listen((data) { print(data); });
+        //process.stdin.writeln('Hello, world!');
+        //process.stdin.writeln('Hello, galaxy!');
+        //process.stdin.writeln('Hello, universe!');
+      });
+    });
+}
